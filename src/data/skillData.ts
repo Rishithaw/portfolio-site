8cc1cbe113ecdc503a25d4a07d40db5821d51aa4
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-11-23 08:42:22 -0600

    Added languages

--- src/data/skillData.ts
+++ src/data/skillData.ts
@@ -1,6 +1,11 @@
 import javaImg from "../assets/Java.png";
+import mysqlImg from "../assets/MySQL.png";
+import htmlImg from "../assets/HTML.png";
+import cssImg from "../assets/CSS.png";
+import javascriptImg from "../assets/JavaScript.png";
 import pythonImg from "../assets/Python.png";
 import reactImg from "../assets/React.png";
+import rubyImg from "../assets/Ruby.png";
 
 export const skills = [
   {
@@ -8,6 +13,30 @@ export const skills = [
     description: "Object-oriented backend development",
     image: javaImg,
     link: "https://www.java.com/",
+  },
+  {
+    name: "MySQL",
+    description: "Relational database management",
+    image: mysqlImg,
+    link: "https://www.mysql.com/",
+  },
+  {
+    name: "HTML",
+    description: "Structure and content markup",
+    image: htmlImg,
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    name: "CSS",
+    description: "Styling and layout design",
+    image: cssImg,
+    link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    name: "JavaScript",
+    description: "Interactive web functionality",
+    image: javascriptImg,
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
     name: "Python",
@@ -21,5 +50,10 @@ export const skills = [
     image: reactImg,
     link: "https://react.dev/",
   },
-  // add more...
+  {
+    name: "Ruby",
+    description: "Web development with Rails",
+    image: rubyImg,
+    link: "https://www.ruby-lang.org/",
+  }
 ];
