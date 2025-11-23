146c6494efe0544b0f8752cef680389217dd9b39
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-11-22 18:19:31 -0600

    Added Skills

--- src/data/skillData.ts
+++ src/data/skillData.ts
@@ -1,1 +1,5 @@
-export {};
+export default {
+  languages: ['Java','MySql','JavaFx','HTML','CSS','JavaScript', 'Python', 'Ruby','JavaScript', 'TypeScript', 'PostgreSQL'],
+  frameworks: ['React', 'Express'],
+  tools: ['Git', 'Docker', 'Storybook', 'VSCode', 'Figma', 'draw.io']
+};
--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,3 +1,20 @@
-export const Skills = () => {
-  return <div>Skills Page</div>;
+import React from 'react';
+import skillData from '../../data/skillData';
+import { Table, TableHeader, TableRow, TableCell, TableFooter } from '../../Components/Table';
+
+export const Skills: React.FC = () => {
+  return (
+    <div>
+      <h1>Skills</h1>
+      <h3>Languages / Frameworks</h3>
+      <ul>
+        {skillData.languages.map((l) => <li key={l}>{l}</li>)}
+      </ul>
+
+      <h3>Tools</h3>
+      <ul>
+        {skillData.tools.map((t) => <li key={t}>{t}</li>)}
+      </ul>
+    </div>
+  );
 };
